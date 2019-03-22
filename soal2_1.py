b29a219ac1be92c7c7060afb710c25695f3b5d61
karinagustina <43054307+karinagustina@users.noreply.github.com>
2019-03-23 02:05:26 +0700

    Jawaban soal2_1

--- soal2_1.py
+++ soal2_1.py
@@ -2,34 +2,53 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+# =========================================
+# Preprocessing
+# =========================================
+
 df = pd.read_csv('data.csv')
-# print(df)
+dfFifa = df[['Name','Age','Overall','Potential']]
+# print(dfFifa)
+dftarget = dfFifa[dfFifa['Age'] <= 25][dfFifa['Overall'] >= 80][dfFifa['Potential'] >= 80]
+# print(dftarget)
+dfnontarget = dfFifa.drop(dftarget.index)
+# print(dfnontarget) 
 
-tAge = df[['Age']][df['Age'] <= 25]
-print(len(tAge))
-tOverall = df[['Overall']][df['Overall'] >= 80]   
-print(len(tOverall))
-tPotential = df[['Potential']][df['Potential'] >= 80]
-
-ntAge = df[['Age']][df['Age'] > 25]  
-ntOverall = df[['Overall']][df['Overall'] < 80]    
-ntPotential = df[['Potential']][df['Potential'] < 80]   
+# =========================================
+# Plotting
+# =========================================
 
 fig = plt.figure('Fifa', figsize = (13,6))
 
+#Plot Age vs Overal
 ax = plt.subplot(121)
-# plt.scatter(
-#     tAge,
-#     tOverall,
-#     color = 'g',
-#     label = 'Target'
-# )
-# plt.scatter(
-#     ntAge,
-#     ntOverall,
-#     color = 'r',
-#     label = 'Non-Target'
-# )
+
+#Plot Age vs Overall (Target)
+xt1 = (dftarget['Age'].values).reshape(-1, 1)
+# print(xt1)
+yt1 = dftarget['Overall'].values
+# print(yt1)
+plt.scatter(
+    xt1,
+    yt1,
+    color = 'g',
+    s = 5,
+    label = 'Target'
+)
+
+#Plot Age vs Overall (Non-Target)
+xt1 = (dfnontarget['Age'].values).reshape(-1, 1)
+# print(xt1)
+yt1 = dfnontarget['Overall'].values
+# print(yt1)
+plt.scatter(
+    xt1,
+    yt1,
+    color = 'r',
+    s = 5,
+    label = 'Non-Target'
+)
+
 ax.legend()
 ax.grid(True)
 ax.set_xlabel('Age')
@@ -38,19 +57,31 @@ ax.set_title('Age vs Overall')
 ax.legend()
 ax.grid(True)
 
+#Plot Age vs Potential
 bx = plt.subplot(122)
+
+#Plot Age vs Potential (Target)
+xt2 = (dftarget['Age'].values).reshape(-1, 1)
+yt2 = dftarget['Potential'].values
 plt.scatter(
-    tAge,
-    tPotential,
+    xt2,
+    yt2,
     color = 'g',
+    s = 5,
     label = 'Target'
 )
+
+#Plot Age vs Potential (Non-Target)
+xt2 = (dfnontarget['Age'].values).reshape(-1, 1)
+yt2 = dfnontarget['Potential'].values
 plt.scatter(
-    ntAge,
-    ntPotential,
+    xt2,
+    yt2,
     color = 'r',
+    s = 5,
     label = 'Non-Target'
 )
+
 bx.set_xlabel('Age')
 bx.set_ylabel('Potential')
 bx.set_title('Age vs Potential')
